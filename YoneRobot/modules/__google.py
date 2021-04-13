28fa00f3207f0d521b31c2510d34470fd7411580
Nksama <75163978+Nksama@users.noreply.github.com>
2021-04-13 07:14:37 +0530

    Update __google.py

--- YoneRobot/modules/__google.py
+++ YoneRobot/modules/__google.py
@@ -286,6 +286,6 @@ __help__ = """
  ❍ /github <username>*:* Get information about a GitHub user.
  ❍ /country <country name>*:* Gathering info about given country
  ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
- ❍ Tomioka <query>*:* Yone answers the query
+ ❍ Tomioka <query>*:* Tomioka answers the query
   💡Ex: `Tomioka what is uwu`
 """
