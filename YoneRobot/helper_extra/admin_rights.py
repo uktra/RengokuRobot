6802129dde833d70a60144842660acca95a72013
Nksama <75163978+Nksama@users.noreply.github.com>
2021-04-13 13:01:28 +0530

    Update admin_rights.py

--- YoneRobot/helper_extra/admin_rights.py
+++ YoneRobot/helper_extra/admin_rights.py
@@ -14,4 +14,4 @@ def user_can_pin(chat: Chat, user: User, bot_id: int) -> bool:
 
 
 def user_can_changeinfo(chat: Chat, user: User, bot_id: int) -> bool:
-    return chat.get_member(user.id).
+    return chat.get_member(user.id)
