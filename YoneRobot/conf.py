7643d1ff0d405ba31f661dd432e3c26f4c048121
Nksama <75163978+Nksama@users.noreply.github.com>
2021-04-13 12:12:49 +0530

    Create conf.py

---
+++ YoneRobot/conf.py
@@ -0,0 +1,20 @@
+from envparse import env
+from YoneRobot import LOGGER
+
+DEFAULTS = {
+    "LOAD_MODULES": True,
+}
+
+def get_str_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.str(name, default=default)) and not required:
+        LOGGER.warn("No str key: " + name)
+        return None
+    elif not data:
+        LOGGER.critical("No str key: " + name)
+        sys.exit(2)
+    else:
+        return data
