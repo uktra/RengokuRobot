9544ec2784fd23a441f43cf65511b241141b3b69
Nksama <75163978+Nksama@users.noreply.github.com>
2021-04-13 12:57:04 +0530

    Update __main__.py

--- YoneRobot/__main__.py
+++ YoneRobot/__main__.py
@@ -683,7 +683,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Fire breathing First form! i am awake)
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Fire breathing First form! i am awake")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
