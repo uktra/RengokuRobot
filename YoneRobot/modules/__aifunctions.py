332b0dcf65e27751ec7df7388febb520d6166199
Nksama <75163978+Nksama@users.noreply.github.com>
2021-04-13 11:55:32 +0530

    Update __aifunctions.py

--- YoneRobot/modules/__aifunctions.py
+++ YoneRobot/modules/__aifunctions.py
@@ -10,7 +10,7 @@ from telethon import types
 from telethon.tl import functions
 import asyncio, os
 
-@register(pattern="Tomioka (.*)")
+@register(pattern="Rengoku (.*)")
 async def hmm(event):
   test = event.pattern_match.group(1)
   r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
@@ -37,7 +37,7 @@ async def hmm(event):
    except CFError as e:
            print(e)
   elif "ann" in result:
-   pro = "My name is Tomioka"
+   pro = "My name is Rengoku"
    try:
       async with telethn.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
